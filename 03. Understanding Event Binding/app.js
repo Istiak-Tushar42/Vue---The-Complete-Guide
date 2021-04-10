370b3bcbd2354d4368ecf49f084bb8c068d52b9e
Istiak-Tushar42 <istiak.tushar.it@gmail.com>
2021-04-10 15:48:48 +0600

    dynamic styling starting

--- 03. Understanding Event Binding/app.js
+++ 03. Understanding Event Binding/app.js
@@ -10,6 +10,16 @@ const app = Vue.createApp({
 	},
 
 	watch: {
+		counter(value) {
+			if(value > 50) {
+				const that = this;
+
+				setTimeout(() => {
+					that.counter = 0;
+				}, 2000);
+			}
+		},
+
 		// name(value) {
 		// 	if(value === '') {
 		// 		this.fullname = '';
@@ -28,12 +38,12 @@ const app = Vue.createApp({
 	},
 
 	computed: {
-		// fullName() {
-		// 	if(this.name === '') {
-		// 		return '';
-		// 	}
-		// 	return this.name + ' ' + 'Tushar';
-		// }
+		fullName() {
+			if(this.name === '' || this.lastName === '') {
+				return '';
+			}
+			return this.name + ' ' + this.lastName;
+		},
 	},
 
 	methods: {
