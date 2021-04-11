2c5b6048dfb032d0c5b289574e497c8d4f13a9f6
Istiak-Tushar42 <istiak.tushar.it@gmail.com>
2021-04-11 20:45:12 +0600

    Project Setup & First Method

--- 03. Course Project The Monster Slayer Game/app.js
+++ 03. Course Project The Monster Slayer Game/app.js
@@ -1,5 +1,27 @@
-const app = Vue.createElement({
-    
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+        };
+    },
+
+    methods: {
+        attackMonster() {
+            const attactValue = getRandomValue(5, 12);
+            this.monsterHealth -= attactValue;
+            this.attackPlayer();
+        },
+
+        attackPlayer() {
+            const attactValue = getRandomValue(8, 15);
+            this.playerHealth -= attactValue;
+        }
+    }
 });
 
 app.mount('#game');
