4fe19b95bc188bd75e3109875970bb6fbf051e81
Istiak-Tushar42 <istiak.tushar.it@gmail.com>
2021-04-11 21:05:39 +0600

    Adding a Special Attack

--- 03. Course Project The Monster Slayer Game/app.js
+++ 03. Course Project The Monster Slayer Game/app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0
         };
     },
 
@@ -14,14 +15,19 @@ const app = Vue.createApp({
         monsterBarStyles() {
             return {width: this.monsterHealth + '%'};
         },
-        
+
         playerBarStyles() {
             return {width: this.playerHealth + '%'};
+        },
+
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     },
 
     methods: {
         attackMonster() {
+            this.currentRound++;
             const attactValue = getRandomValue(5, 12);
             this.monsterHealth -= attactValue;
             this.attackPlayer();
@@ -30,6 +36,13 @@ const app = Vue.createApp({
         attackPlayer() {
             const attactValue = getRandomValue(8, 15);
             this.playerHealth -= attactValue;
+        },
+
+        specialAttackMonster() {
+            this.currentRound++;
+            const attactValue = getRandomValue(10, 25);
+            this.monsterHealth -= attactValue;
+            this.attackPlayer();
         }
     }
 });
