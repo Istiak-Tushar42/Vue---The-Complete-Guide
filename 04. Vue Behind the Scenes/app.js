54906207ba1cd3f73b5b8d2b98572262ff06f73b
Istiak-Tushar42 <istiak.tushar.it@gmail.com>
2021-04-12 21:52:44 +0600

    Working with Refs

--- 04. Vue Behind the Scenes/app.js
+++ 04. Vue Behind the Scenes/app.js
@@ -5,14 +5,15 @@ const app = Vue.createApp({
             message: 'Vue is great!',
         };
     },
-    
+
     methods: {
         saveInput(event) {
             this.currentUserInput = event.target.value;
         },
 
         setText() {
-            this.message = this.currentUserInput;
+            // this.message = this.currentUserInput;
+            this.message = this.$refs.userText.value;
         },
     },
 });
